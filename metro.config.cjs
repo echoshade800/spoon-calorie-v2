1a21aed2645576199e4d5b8efa0edb3ea2695851
echoshade800 <echoshade800@gmail.com>
2025-09-10 14:48:33 +0800

    Fix Metro configuration module error

--- metro.config.cjs
+++ metro.config.cjs
@@ -2,8 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Add support for WebAssembly files
-config.resolver.assetExts.push('wasm');
-config.resolver.sourceExts.push('wasm');
-
 module.exports = config;
