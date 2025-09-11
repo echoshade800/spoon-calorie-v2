d2d5d4c4c0dfa994e65e693d63efa8715d4f9ee1
echoshade800 <echoshade800@gmail.com>
2025-09-11 14:38:16 +0800

    Fix Metro file watcher error

--- metro.config.cjs
+++ metro.config.cjs
@@ -2,4 +2,22 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Fix file watcher issues
+config.watchFolders = [];
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Disable file watching for problematic directories
+config.watcher = {
+  additionalExts: ['cjs', 'mjs'],
+  watchman: {
+    deferStates: ['hg.update'],
+  },
+};
+
+// Add transformer options to prevent watch mode issues
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+};
+
 module.exports = config;
